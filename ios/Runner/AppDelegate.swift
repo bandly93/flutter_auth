bfe18e3db2d965ea4422fbdf7db879374f52ae94
Band Ly <bandly93@gmail.com>
2019-10-21 21:54:24 -0700

    updated repo

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,13 @@
 import UIKit
 import Flutter
 import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
-class LandingPage extends StatelessWidget{
+class LandingPage extends StatefulWidget{
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>{
+
+  bool isLogginIn = false;
+ 
 
   Widget buildButton(context,width,text,textStyle,route,color){
     return SizedBox(
@@ -16,6 +24,32 @@ class LandingPage extends StatelessWidget{
     );
   }
 
+  void onLoginStatusChanged(bool isLoggedIn) {
+    setState(() {
+      isLoggedIn = isLoggedIn;
+    });
+  }
+
+  Future<void> signInWithFacebook() async {
+
+    final facebookLogin = FacebookLogin();
+    final result = await facebookLogin.logInWithReadPermissions(['email']);
+    switch (result.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("LoggedIn");
+        onLoginStatusChanged(true);
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -28,9 +62,37 @@ class LandingPage extends StatelessWidget{
             Text('Your Mobile Abstract Diary'),
             Column (
               children: <Widget>[
-                buildButton(context, 300, "SIGN UP", TextStyle(color : Colors.white), '/signup', Colors.transparent),
-                buildButton(context, 300, "CONTINUE WITH FACEBOOK", TextStyle(color:Colors.white), '/login', Colors.blue),
-                buildButton(context, 300, "LOG IN ", TextStyle(color:Colors.black), '/login', Colors.white),
+                SizedBox(
+                  width : 300,
+                  child : RaisedButton(
+                    color: Colors.transparent,
+                    shape : RoundedRectangleBorder(borderRadius : new BorderRadius.circular(30.0)),
+                    onPressed : (){
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child : Text("Sign Up" , style : TextStyle(color : Colors.white)),
+                  ),
+                ),
+                SizedBox(
+                  width : 300,
+                  child : RaisedButton(
+                    color: Colors.blue,
+                    shape : RoundedRectangleBorder(borderRadius : new BorderRadius.circular(30.0)),
+                    onPressed : signInWithFacebook,
+                    child : Text("CONTINUE WITH FACEBOOK" , style : TextStyle(color:Colors.white)),
+                  ),
+                ),
+                SizedBox(
+                  width : 300,
+                  child : RaisedButton(
+                    color: Colors.white,
+                    shape : RoundedRectangleBorder(borderRadius : new BorderRadius.circular(30.0)),
+                    onPressed : (){
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child : Text("LOG IN" , style : TextStyle(color:Colors.black)),
+                  ),
+                ),
               ],
             )
           ],
